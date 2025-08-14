9ac3bd8734a7891f7c5d806482ea19827080d561
Tributino03 <tributino2705@gmail.com>
2025-08-13 23:11:49 -0300

    feat: Adiciona Lombok e Log4J2

--- src/main/java/repository/ProducerRepository.java
+++ src/main/java/repository/ProducerRepository.java
@@ -2,18 +2,20 @@ package repository;
 
 import dominio.Producer;
 import jdbc.ConnectionFactory;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@Log4j2
 public class ProducerRepository {
     public static void save(Producer producer) {
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName());
         try (Connection conn = ConnectionFactory.getConnection();
              Statement stmt = conn.createStatement()) {
             int rowsAffected = stmt.executeUpdate(sql);
-            System.out.println(rowsAffected);
+            log.info("Inserted producer in the database, rows affected '{}'", rowsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/test/java/ConnectionFactoryTest01.java
+++ src/test/java/ConnectionFactoryTest01.java
@@ -1,14 +1,17 @@
 import dominio.Producer;
 import jdbc.ConnectionFactory;
+import lombok.extern.log4j.Log4j2;
 import repository.ProducerRepository;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
     }
 }
