3ff9ebeeb8f117ec6d4de921d0da77360a03df26
Tributino03 <tributino2705@gmail.com>
2025-08-14 19:07:59 -0300

    feat: adiciona DatabaseMetaData

--- src/test/java/ConnectionFactoryTest01.java
+++ src/test/java/ConnectionFactoryTest01.java
@@ -14,13 +14,14 @@ import java.util.List;
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
-        Producer producerToUpdate = Producer.builder().id(10).name("MADHOUSE").build();
+        Producer producerToUpdate = Producer.builder().id(1).name("MADHOUSE").build();
 //        ProducerService.save(producer);
 //        ProducerService.delete(5);
 //        ProducerService.update(producerToUpdate);
 //        List<Producer> producers = ProducerService.findAll();
 //        List<Producer> producers = ProducerService.findByName("Mad");
 //        log.info("Producers found '{}'", producers);
-        ProducerService.showProducerMetadata();
+//        ProducerService.showProducerMetaData();
+        ProducerService.showDriverMetaData();
     }
 }
