963f69304e6b48b9107f986779f5e7bc473d04df
Tributino03 <tributino2705@gmail.com>
2025-08-13 23:52:18 -0300

    feat: deleta dados com Statement

--- src/test/java/ConnectionFactoryTest01.java
+++ src/test/java/ConnectionFactoryTest01.java
@@ -2,6 +2,7 @@ import dominio.Producer;
 import jdbc.ConnectionFactory;
 import lombok.extern.log4j.Log4j2;
 import repository.ProducerRepository;
+import service.ProducerService;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -12,6 +13,13 @@ import java.sql.Statement;
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
         Producer producer = Producer.builder().name("Studio Deen").build();
-        ProducerRepository.save(producer);
+        //ProducerService.save(producer);
+        ProducerService.delete(4);
+        ProducerService.delete(5);
+        ProducerService.delete(6);
+        ProducerService.delete(7);
+        ProducerService.delete(8);
+        ProducerService.delete(9);
+
     }
 }
