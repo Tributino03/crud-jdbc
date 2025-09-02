f637b6528ef35412ff8ffe8d2b524faaaa94b33b
Tributino03 <tributino2705@gmail.com>
2025-09-02 11:23:20 -0300

    feat: adiciona CachedRowSet

--- src/main/java/jdbc/ConnectionFactory.java
+++ src/main/java/jdbc/ConnectionFactory.java
@@ -1,6 +1,7 @@
 package jdbc;
 
 
+import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.JdbcRowSet;
 import javax.sql.rowset.RowSetProvider;
 import java.sql.Connection;
@@ -25,5 +26,9 @@ public class ConnectionFactory {
         jdbcRowSet.setUsername(username);
         jdbcRowSet.setPassword(password);
         return jdbcRowSet;
+    }
+
+    public static CachedRowSet getCachedRowSet() throws SQLException {
+        return RowSetProvider.newFactory().createCachedRowSet();
     }
 }
