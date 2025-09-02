bc981cea43333138bee73d1971f94ebb80808166
Tributino03 <tributino2705@gmail.com>
2025-09-02 10:45:29 -0300

    feat: adiciona JdbcRowSet

---
+++ src/main/java/service/ProducerServiceRowSet.java
@@ -0,0 +1,17 @@
+package service;
+
+import dominio.Producer;
+import repository.ProducerRepositoryRowSet;
+
+import java.util.List;
+
+public class ProducerServiceRowSet {
+
+    public static List<Producer> findByNameJdbcRowSet(String name) {
+        return ProducerRepositoryRowSet.findByNameJdbcRowSet(name);
+    }
+
+    public static void updateJdbcRowSet(Producer producer) {
+        ProducerRepositoryRowSet.updateJdbcRowSet(producer);
+    }
+}
---
+++ src/test/java/ConnectionFactoryTest02.java
@@ -0,0 +1,16 @@
+import dominio.Producer;
+import lombok.extern.log4j.Log4j2;
+import service.ProducerServiceRowSet;
+
+import java.util.List;
+
+@Log4j2
+public class ConnectionFactoryTest02 {
+    public static void main(String[] args) {
+        Producer producerToUpdate = Producer.builder().id(1).name("MAD").build();
+        ProducerServiceRowSet.updateJdbcRowSet(producerToUpdate);
+        log.info("------------------------");
+        List<Producer> producers = ProducerServiceRowSet.findByNameJdbcRowSet("");
+        log.info(producers);
+    }
+}
