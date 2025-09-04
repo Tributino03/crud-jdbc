5129a25f4a3b5f62a0a9d3286080eeccd4f58833
Tributino03 <tributino2705@gmail.com>
2025-09-04 11:52:08 -0300

    feat(jUnit): adiciona testes unitarios

---
+++ src/main/java/junit/test/PersonServiceTest01.java
@@ -0,0 +1,15 @@
+package junit.test;
+
+import junit.dominio.Person;
+import junit.service.PersonService;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class PersonServiceTest01 {
+    public static void main(String[] args) {
+        Person person = new Person(15);
+        PersonService personService = new PersonService();
+        log.info("Is Adult? '{}'", personService.isAdult(person));
+
+    }
+}
